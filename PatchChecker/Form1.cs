41743285430a3d9a53e81388e79bc1afbb80dfb7
Eitan Herman <eitan@securegion.com>
2017-05-17 11:01:26 +0300

    fixes

--- PatchChecker/Form1.cs
+++ PatchChecker/Form1.cs
@@ -1,4 +1,4 @@
-﻿using SRLib.Service.Impl;
+﻿
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,6 +9,7 @@ using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace PatchChecker
 {
@@ -22,46 +23,12 @@ namespace PatchChecker
         private void button1_Click(object sender, EventArgs e)
         {
             // MessageBox.Show(username.Text+ " "+password.Text);
-            getPatches("192.168.59.131", username.Text, password.Text);
-        }
-
-        private void getPatches(String host, String user, String password)
-        {
-            //SRLib.Service.SoftwareService software = new SRLib.Service.SoftwareService();
-            //List<SRLib.Model.InstalledAppObject> apps= software.GetInstalledApps("192.168.59.131", "administrator", "10ligisha!");
-            //foreach (SRLib.Model.InstalledAppObject app in apps)
-            // {
-            //    Console.WriteLine(app.Name);
-            //}
-
-            //do your code here
-            WmiAction wmi = new WmiAction(host, user, password);
-            if (!wmi.Connect()) return;
-
-            List<HotfixInfo> list = new List<HotfixInfo>();
-            using (var searcher = wmi.CreateSearcher("select * from Win32_QuickFixEngineering"))
-            using (var collection = searcher.Get())
-                foreach (var item in collection)
-                    using (item)
-                    {
-                        string source = (string)item["CSName"];
-                        string hotFixId = (string)item["HotFixID"];
-                        string description = (string)item["Description"];
-                        string installedBy = (string)item["InstalledBy"];
-                        string installDate = (string)item["InstalledOn"];
-                        //if (installDate != null)
-                        //    installDate = ManagementDateTimeConverter.ToDateTime(installDate).ToString("yyyy-MM-dd HH:mm:ss");
-                        list.Add(new HotfixInfo
-                        {
-                            Source = source,
-                            HotfixId = hotFixId,
-                            Description = description,
-                            InstalledBy = installedBy,
-                            InstallDate = installDate
-                        });
-                    }
+            PatchChecker.Service.Patches patches = new Service.Patches();
+            List<HotfixInfo> list=patches.getPatches("192.168.59.131", username.Text, password.Text);
             table1.DataSource = list;
         }
+
+        
 
 
 
--- PatchChecker/Service/Patches.cs
+++ PatchChecker/Service/Patches.cs
@@ -3,19 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SRLib.Service.Impl;
 
 namespace PatchChecker.Service
 {
     class Patches
     {
-        public void getPatches()
+        public List<HotfixInfo> getPatches(String host, String user, String password)
         {
-            SRLib.Service.SoftwareService software = new SRLib.Service.SoftwareService();
-            List<SRLib.Model.InstalledAppObject> apps = software.GetInstalledApps("192.168.59.131", "administrator", "10ligisha!");
-            foreach (SRLib.Model.InstalledAppObject app in apps)
-            {
-                Console.WriteLine(app.Name);
-            }
+            //SRLib.Service.SoftwareService software = new SRLib.Service.SoftwareService();
+            //List<SRLib.Model.InstalledAppObject> apps= software.GetInstalledApps("192.168.59.131", "administrator", "10ligisha!");
+            //foreach (SRLib.Model.InstalledAppObject app in apps)
+            // {
+            //    Console.WriteLine(app.Name);
+            //}
+
+            //do your code here
+            WmiAction wmi = new WmiAction(host, user, password);
+            if (!wmi.Connect()) return null;
+
+            List<HotfixInfo> list = new List<HotfixInfo>();
+            using (var searcher = wmi.CreateSearcher("select * from Win32_QuickFixEngineering"))
+            using (var collection = searcher.Get())
+                foreach (var item in collection)
+                    using (item)
+                    {
+                        string source = (string)item["CSName"];
+                        string hotFixId = (string)item["HotFixID"];
+                        string description = (string)item["Description"];
+                        string installedBy = (string)item["InstalledBy"];
+                        string installDate = (string)item["InstalledOn"];
+                        //if (installDate != null)
+                        //    installDate = ManagementDateTimeConverter.ToDateTime(installDate).ToString("yyyy-MM-dd HH:mm:ss");
+                        list.Add(new HotfixInfo
+                        {
+                            Source = source,
+                            HotfixId = hotFixId,
+                            Description = description,
+                            InstalledBy = installedBy,
+                            InstallDate = installDate
+                        });
+                    }
+            return list;
+           // table1.DataSource = list;
         }
+
     }
 }
